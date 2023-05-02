be81cb56454e3977d2b4460b5206e3e073351834
Griny34 <43299@mail.ru>
2023-05-02 09:08:58 +0300

    Change 1

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _prefab;
+    [SerializeField] private EnemyMovement _prefab;
     [SerializeField] private Transform[] _spawnerPoint;
     [SerializeField] private int _spawnTime;
 
